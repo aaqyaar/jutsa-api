3c0715d1782e6f111c829731b509f804335da543
abdizamedmo <abdizamedmo@gmail.com>
2023-12-12 11:11:33 +0300

    Config module added

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  app.setGlobalPrefix('api');
+
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<number>('port'));
+}
+bootstrap();
