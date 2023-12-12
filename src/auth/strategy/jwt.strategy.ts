9d677c9f596545e7923f53a3b34e46dde8e62a55
abdizamedmo <abdizamedmo@gmail.com>
2023-12-12 11:09:57 +0300

    Using JWT to authenticate users

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { JwtPayload } from 'src/types/auth';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    const secret = configService.get<string>('jwtSecret');
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: secret,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    return {
+      id: payload.id,
+      name: payload.name,
+      role: payload.role,
+    };
+  }
+}
